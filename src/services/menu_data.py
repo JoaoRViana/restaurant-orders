76030d860597c45ef752430561f9f1d083873ee9
João Ricardo Viana <joaorviana98@gmail.com>
2023-09-04 13:45:33 -0300

    req03

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,5 +1,39 @@
+import csv 
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+
 # Req 3
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        
-        pass
+        self.source_path = source_path
+        self.dishes = set(self.get_dishes())
+    
+    def get_dishes(self):
+        with open(self.source_path, 'r') as archive:
+            list_dishes = csv.DictReader(archive)
+            dishes = []
+
+            for i in list_dishes:
+                name = i['dish']
+                price = float(i['price'])
+                ingredient = i['ingredient']
+                amount = int(i['recipe_amount'])
+                for j in dishes:
+                    ingredient_class = Ingredient(ingredient)
+                    if j.name != name:
+                        dish_class = Dish(name, price)
+                        dish_class.add_ingredient_dependency(
+                            ingredient_class, amount)
+                        dishes.append(dish_class)
+                    else:
+                        j.add_ingredient_dependency(
+                            ingredient_class, amount)
+                if len(dishes) < 1:
+                    ingredient_class = Ingredient(ingredient)
+                    dish_class = Dish(name, price)
+                    dish_class.add_ingredient_dependency(
+                            ingredient_class, amount)
+                    dishes.append(dish_class)
+            return dishes
